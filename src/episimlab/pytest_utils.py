14baf504300685e7e9f417750bbe15170a9923f4
Ethan Ho <ethan.n.ho@gmail.com>
2020-10-21 12:24:15 -0500

    Add wall clock timer to profiling

--- src/episimlab/pytest_utils.py
+++ src/episimlab/pytest_utils.py
@@ -5,9 +5,10 @@ import numpy as np
 from dask.diagnostics import ResourceProfiler, Profiler, ProgressBar
 from functools import wraps
 import datetime
+import time
 import logging
 
-def dask_prof(log_dir=None, log_stub=None, show_prof=True):
+def dask_prof(log_dir=None, log_stub=None, show_prof=False):
     """Decorates `func` with Dask memory and thread profiling. This function
     returns a decorator, so use like:
 
@@ -28,13 +29,15 @@ def dask_prof(log_dir=None, log_stub=None, show_prof=True):
     def decorator(func):
         @wraps(func)
         def with_prof(*args, **kwargs):
+            start = time.time()
             with ResourceProfiler(dt=0.25) as rprof, Profiler() as tprof:
                 result = func(*args, **kwargs)
-            if log_dir is not None:
-                rprof.visualize(file_path=base_fp(name='rprof', ext='html'),
-                                show=show_prof, save=True)
-                tprof.visualize(file_path=base_fp(name='tprof', ext='html'),
-                                show=show_prof, save=True)
+            rprof.visualize(file_path=base_fp(name='rprof', ext='html'),
+                            show=show_prof, save=True)
+            tprof.visualize(file_path=base_fp(name='tprof', ext='html'),
+                            show=show_prof, save=True)
+            elapsed = time.time() - start
+            logging.debug(f"'{func.__name__}' took {elapsed:0.2f} seconds")
             return result
         return with_prof
     return decorator
