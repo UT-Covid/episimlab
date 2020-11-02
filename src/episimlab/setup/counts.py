ebc3d77e1c41112453df1eb285036ef25873fb8a
Ethan Ho <ethan.n.ho@gmail.com>
2020-11-02 15:11:08 -0600

    Default counts array starts with realistic populations

--- src/episimlab/setup/counts.py
+++ src/episimlab/setup/counts.py
@@ -16,14 +16,28 @@ class InitDefaultCounts:
     vertex = xs.foreign(InitDefaultCoords, 'vertex')
 
     def initialize(self):
-        self.counts = xr.DataArray(
+        # self.counts = self.ones_counts_basic()
+        self.counts = self.realistic_counts_basic()
+
+    def ones_counts_basic(self):
+        return xr.DataArray(
             data=1.,
             dims=self.COUNTS_DIMS,
-            coords={
-                'age_group': self.age_group,
-                'risk_group': self.risk_group,
-                'compartment': self.compartment,
-                'vertex': self.vertex
-            }
+            coords={dim: getattr(self, dim) for dim in self.COUNTS_DIMS}
         )
+
+    def realistic_counts_basic(self):
+        da = xr.DataArray(
+            data=0.,
+            dims=self.COUNTS_DIMS,
+            coords={dim: getattr(self, dim) for dim in self.COUNTS_DIMS}
+        )
+        # Houston
+        da.loc[dict(vertex=0, compartment='S')] = 2.326e6 / 10.
+        # Austin
+        da.loc[dict(vertex=1, compartment='S')] = 1e6 / 10.
+        # Beaumont
+        da.loc[dict(vertex=2, compartment='S')] = 1.18e5 / 10.
+
+        return da
 
