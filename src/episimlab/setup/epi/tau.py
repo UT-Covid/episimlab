700bb69f43d0257f2975b0e0108ddbb8f7f651c9
Ethan Ho <eho@tacc.utexas.edu>
2020-11-30 13:13:27 -0600

    Add more advanced setup processes for tau

--- src/episimlab/setup/epi/tau.py
+++ src/episimlab/setup/epi/tau.py
@@ -9,7 +9,7 @@ from .base import BaseSetupEpi
 
 @xs.process
 class SetupDefaultTau(BaseSetupEpi):
-    """
+    """Return a default value for tau.
     """
 
     tau = xs.foreign(BaseSEIR, 'tau', intent='out')
@@ -19,3 +19,14 @@ class SetupDefaultTau(BaseSetupEpi):
 
     def get_tau(self):
         return 0.57
+
+
+@xs.process
+class SetupTauFromAsympRate(SetupDefaultTau):
+    """Given a static scalar input asymptomatic ratio, calculate tau.
+    """
+
+    asymp_rate = xs.variable(dims=(), static=True, intent='in')
+
+    def get_tau(self):
+        return 1 - self.asymp_rate
