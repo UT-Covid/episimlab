2243e2fb0e3bcb77acdae26e57f25590534aef42
Ethan Ho <eho@tacc.utexas.edu>
2021-01-26 13:06:29 -0700

    Aggregate into a single test
    For efficiency

--- tests/test_models/test_toy.py
+++ tests/test_models/test_toy.py
@@ -18,9 +18,6 @@ def input_vars(config_dict, config_fp):
 
 
 class TestToyModels:
-    """
-    TODO: cache model results
-    """
 
     def run_model(self, model, step_clock, input_vars, output_vars):
         input_ds = xs.create_setup(
@@ -36,15 +33,22 @@ class TestToyModels:
         toy.slow_seir(),
         toy.slow_seir_cy_foi(),
         toy.cy_seir_cy_foi(),
-        # toy.cy_adj_slow_seir(),
-        # Travel only does not change net S compt
-        # toy.cy_adj()
     ))
-    def test_can_change_S(self, epis, model, input_vars, counts_basic,
-                          output_vars, step_clock):
+    def test_sanity(self, epis, model, input_vars, counts_basic, output_vars,
+                    step_clock):
+        """Tests models with a handful of sanity checks."""
         result = self.run_model(model, step_clock, input_vars, output_vars)
         assert isinstance(result, xr.Dataset)
         counts = result['apply_counts_delta__counts']
+
+        # ensure that no coords are null
+        for coord in result.coords.values():
+            assert not coord.isnull().any()
+
+        # ensure that the total population has not changed between
+        # first and last timepoints
+        net_change = (counts[dict(step=0)] - counts[dict(step=-1)]).sum()
+        assert abs(net_change) <= 1e-8
 
         # ensure that S compt has changed between first and last timesteps
         S_init = counts[dict(step=0)].loc[dict(compartment="S")]
@@ -52,58 +56,7 @@ class TestToyModels:
         S_change = (S_final - S_init).sum()
         assert abs(S_change) > 1e-8
 
-    # @profiler()
-    @pytest.mark.parametrize('model', (
-        toy.slow_seir(),
-        toy.slow_seir_cy_foi(),
-        toy.cy_seir_cy_foi(),
-        # These Cython implementations do not report FOI
-        # toy.cy_adj_slow_seir(),
-        # toy.cy_adj()
-    ))
-    def test_non_zero_foi(self, epis, model, input_vars, counts_basic,
-                           output_vars, step_clock):
-        output_vars['foi__foi'] = 'step'
-        result = self.run_model(model, step_clock, input_vars, output_vars)
-        assert isinstance(result, xr.Dataset)
-
         # NOTE: will break if no FOI is reported
         # ensure non-zero force of infection at first timepoint
         foi_init = result['foi__foi'][dict(step=1)].sum()
         assert foi_init > 1e-8
-
-    # @profiler()
-    @pytest.mark.parametrize('model', (
-        toy.slow_seir(),
-        toy.slow_seir_cy_foi(),
-        toy.cy_seir_cy_foi(),
-        # toy.cy_adj_slow_seir(),
-        # toy.cy_adj()
-    ))
-    def test_constant_pop(self, epis, model, input_vars, counts_basic,
-                           output_vars, step_clock):
-        result = self.run_model(model, step_clock, input_vars, output_vars)
-        assert isinstance(result, xr.Dataset)
-        counts = result['apply_counts_delta__counts']
-
-        # ensure that the total population has not changed between
-        # first and last timepoints
-        net_change = (counts[dict(step=0)] - counts[dict(step=-1)]).sum()
-        assert abs(net_change) <= 1e-8
-
-    # @profiler()
-    @pytest.mark.parametrize('model', (
-        toy.slow_seir(),
-        toy.slow_seir_cy_foi(),
-        toy.cy_seir_cy_foi(),
-        # toy.cy_adj_slow_seir(),
-        # toy.cy_adj()
-    ))
-    def test_non_null_coords(self, epis, model, input_vars, counts_basic,
-                             output_vars, step_clock):
-        result = self.run_model(model, step_clock, input_vars, output_vars)
-        counts = result['apply_counts_delta__counts']
-
-        # ensure that no coords are null
-        for coord in result.coords.values():
-            assert not coord.isnull().any()
