3a640ce8d70fb5275cfa987cdcda0198cfea5298
Ethan Ho <eho@tacc.utexas.edu>
2021-02-10 10:47:59 -0700

    Revert to use index instead of variable

--- src/episimlab/setup/coords.py
+++ src/episimlab/setup/coords.py
@@ -8,10 +8,10 @@ class InitDefaultCoords:
     in the counts array (except time).
     """
 
-    age_group = xs.variable(groups=['coords'], dims='age_group', global_name='age_group', intent='out')
-    risk_group = xs.variable(groups=['coords'], dims='risk_group', global_name='risk_group', intent='out')
-    compartment = xs.variable(groups=['coords'], dims='compartment', global_name='compartment', intent='out')
-    vertex = xs.variable(groups=['coords'], dims='vertex', global_name='vertex', intent='out')
+    age_group = xs.index(groups=['coords'], dims='age_group', global_name='age_group')
+    risk_group = xs.index(groups=['coords'], dims='risk_group', global_name='risk_group')
+    compartment = xs.index(groups=['coords'], dims='compartment', global_name='compartment')
+    vertex = xs.index(groups=['coords'], dims='vertex', global_name='vertex')
 
     def initialize(self):
         self.age_group = ['0-4', '5-17', '18-49', '50-64', '65+']
