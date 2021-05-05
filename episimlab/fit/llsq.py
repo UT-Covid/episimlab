f6a9186544997f28755bb2a83dd7d2a2c9740cf3
Ethan Ho <eho@tacc.utexas.edu>
2021-05-05 12:14:21 -0600

    Test using class only

--- episimlab/fit/llsq.py
+++ episimlab/fit/llsq.py
@@ -14,7 +14,7 @@ from xsimlab.model import Model
 
 @attr.s
 class LeastSqFitter:
-    """Wrapper class for running episimlab models with
+    """Base wrapper class for running episimlab models with
     scipy.optimize.least_squares
     """
     data = attr.ib(type=xr.DataArray, repr=False)
@@ -51,8 +51,8 @@ class LeastSqFitter:
             x0=self.guess,
             xtol=1e-8,
             verbose=self.verbosity,
-            # bounds=bounds,
             args=(self.data,),
+            # bounds=bounds,
             **self.ls_kwargs
         )
         return self.soln
@@ -90,84 +90,3 @@ class LeastSqFitter:
         # Calculate residual
         resi = ih_pred - ih_actual
         return resi
-
-
-
-def get_ih_actual() -> xr.DataArray:
-    # data = 0.
-    # da = xr.DataArray(
-    #     data=data,
-    #     dims=list(),
-    #     coords={
-    #         'step'
-    #     }
-    # )
-    return 0.
-
-
-def fit_llsq():
-    data = get_ih_actual()
-    model = basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
-    step_clock = pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H')
-    return (
-        LeastSqFitter(
-            model=model, 
-            data=data, 
-            step_clock=step_clock, 
-            guess=0.035, 
-            verbosity=2)
-        .fit()
-    )
-
-
-def __fit_llsq():
-    soln = least_squares(
-        fun=run_toy_model,
-        x0=0.035,
-        # x_scale=x_scale,
-        xtol=1e-8,  # default
-        verbose=2,
-        # bounds=bounds,
-        args=(get_ih_actual(),)
-    )
-    return soln
-
-
-def run_toy_model(dep_vars, ih_actual) -> float:
-    beta = dep_vars[0]
-
-    # get config
-    config_fp = os.path.join(EPISIMLAB_HOME, 'tests', 'config', 'example_v1.yaml')
-    assert os.path.isfile(config_fp)
-
-    # run model
-    model = basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
-    in_ds = xs.create_setup(
-        model=model,
-        clocks={
-            'step': pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H')
-        },
-        input_vars={
-            'read_config__config_fp': config_fp,
-            'foi__beta': beta
-        },
-        output_vars={
-            'apply_counts_delta__counts': 'step'
-        }
-    )
-    # with ResourceProfiler(dt=1.) as rprof:
-    out_ds = in_ds.xsimlab.run(model=model, decoding=dict(mask_and_scale=False))
-
-    # Pull out counts of Ih compartment over time
-    ih_pred = (out_ds 
-               .apply_counts_delta__counts 
-               .loc[dict(compartment='Ih')] 
-               .sum(dim=['age_group', 'risk_group', 'vertex']))
-    assert len(ih_pred.shape) == 1, (ih_pred.shape, "!= 1")
-    assert 'step' in ih_pred.dims, f"'step' is not in {ih_pred.dims}"
-
-    # Calculate residual
-    resi = ih_pred - ih_actual
-    
-    # breakpoint()
-    return resi
--- tests/test_fit/test_llsq.py
+++ tests/test_fit/test_llsq.py
@@ -1,11 +1,23 @@
 import numpy as np
+import pandas as pd
 import xarray as xr
-from episimlab.fit import llsq
 from scipy.optimize.optimize import OptimizeResult
+from episimlab.fit.llsq import LeastSqFitter
+from episimlab.models import basic as basic_models
 
 
 def test_fit_llsq():
-    result = llsq.fit_llsq()
+    model = basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
+    step_clock = pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H')
+    result = (
+        LeastSqFitter(
+            model=model, 
+            data=0., 
+            step_clock=step_clock, 
+            guess=0.035, 
+            verbosity=2)
+        .fit()
+    )
     assert isinstance(result, OptimizeResult)
     assert result['success']
     assert result['x'] < 1e-7 
