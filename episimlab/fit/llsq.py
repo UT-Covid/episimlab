2b65fa1cbf1712914ad5b748137aa51ce1605136
Ethan Ho <eho@tacc.utexas.edu>
2021-05-05 11:15:49 -0600

    Add LeastSqFitter as attr class

--- episimlab/fit/llsq.py
+++ episimlab/fit/llsq.py
@@ -1,13 +1,44 @@
 """Infrastructure for fitting using linear least squares."""
 
 import os
+import attr
 import xarray as xr
 import pandas as pd
+from pandas.core.indexes.datetimes import DatetimeIndex
 from scipy.optimize import least_squares
 from episimlab import EPISIMLAB_HOME
 from episimlab.models import basic as basic_models
 import xsimlab as xs
 from xsimlab.model import Model
+
+
+@attr.s
+class LeastSqFitter:
+    """Wrapper class for running episimlab models with
+    scipy.optimize.least_squares
+    """
+
+    model = attr.ib(type=Model, repr=True)
+    step_clock = attr.ib(type=DatetimeIndex, repr=True)
+    data = attr.ib(type=xr.DataArray, repr=False)
+    dep_var = attr.ib(type=str, default='beta', repr=True)
+    guess = attr.ib(type=float, default=0., repr=True)
+    # extra kwargs to pass to least_squares
+    ls_kwargs = attr.ib(type=dict, default=attr.Factory(dict), repr=False)
+
+    def fit(self):
+        self.soln = least_squares(
+            fun=run_toy_model,
+            x0=0.035,
+            # x_scale=x_scale,
+            xtol=1e-8,
+            verbose=2,
+            # bounds=bounds,
+            args=(self.data,),
+            **self.ls_kwargs
+        )
+        return self.soln
+
 
 
 def get_ih_actual() -> xr.DataArray:
@@ -23,6 +54,23 @@ def get_ih_actual() -> xr.DataArray:
 
 
 def fit_llsq():
+    data = get_ih_actual()
+    model = basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
+    step_clock = pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H')
+    fitter = LeastSqFitter(model=model, step_clock=step_clock, data=data)
+    soln = least_squares(
+        fun=run_toy_model,
+        x0=0.035,
+        # x_scale=x_scale,
+        xtol=1e-8,  # default
+        verbose=2,
+        # bounds=bounds,
+        args=(get_ih_actual(),)
+    )
+    return soln
+
+
+def __fit_llsq():
     soln = least_squares(
         fun=run_toy_model,
         x0=0.035,
