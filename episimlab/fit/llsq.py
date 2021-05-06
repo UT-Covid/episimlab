f4a6c53af260ba7129a901fd5740be06438a00ea
Ethan Ho <eho@tacc.utexas.edu>
2021-05-06 10:20:37 -0600

    No subclassing, that was messy

--- episimlab/fit/llsq.py
+++ episimlab/fit/llsq.py
@@ -13,74 +13,79 @@ from xsimlab.model import Model
 
 
 @attr.s
-class LeastSqFitter:
-    """Base wrapper class for running episimlab models with
-    scipy.optimize.least_squares
+class FitBetaFromHospHeads:
     """
-    data = attr.ib(type=xr.DataArray, repr=False)
-    guess = attr.ib(type=float)
+    """
+    data_fp = attr.ib(type=str)
     model = attr.ib(type=Model)
+    guess = attr.ib(type=float, default=0.035)
 
-    dep_var = attr.ib(type=str, default='beta')
-    step_clock = attr.ib(type=DatetimeIndex, default=pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H'), repr=False)
+    step_clock = attr.ib(
+        type=DatetimeIndex, repr=False,
+        default=pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H'))
+    config_fp = attr.ib(
+        type=str, repr=True,
+        default=os.path.join(EPISIMLAB_HOME, 'tests', 'config', 'example_v1.yaml'))
     verbosity = attr.ib(type=int, default=0)
     # extra kwargs to pass to least_squares
     ls_kwargs = attr.ib(type=dict, default=attr.Factory(dict), repr=False)
 
-    # TODO: move to child
-    config_fp = attr.ib(type=str, default=os.path.join(EPISIMLAB_HOME, 'tests', 'config', 'example_v1.yaml'), repr=True)
+    @model.default
+    def get_default_model(self):
+        """Generate default value for attrib `model`."""
+        return basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
+    
+    def get_data(self) -> xr.DataArray:
+        """Loads hospitalization event data from CSV at `data_fp`.
+        Returns the Series as a DataArray.
+        """
+        df = pd.read_csv(self.data_fp, comment='#')
+        df.set_index(['zip_code', 'date'], inplace=True)
+        return xr.DataArray.from_series(df['id']).fillna(0.)
 
-    def get_in_ds(self):
-        return xs.create_setup(
-            model=self.model,
-            clocks={
-                'step': self.step_clock
-            },
-            input_vars={
-                'read_config__config_fp': self.config_fp,
-                'foi__beta': beta
-            },
-            output_vars={
-                'apply_counts_delta__counts': 'step'
-            }
-        )
-
-    def fit(self):
+    def run(self):
+        # get data
+        data = self.get_data()
+        
+        # run fitter
         self.soln = least_squares(
             fun=self.calc_residual,
             x0=self.guess,
-            xtol=1e-8,
+            # xtol=1e-8,
+            # DEBUG
+            xtol=1e-3,
             verbose=self.verbosity,
-            args=(self.data,),
+            # args=(data,),
             # bounds=bounds,
             **self.ls_kwargs
         )
         return self.soln
-    
-    def get_in_ds(self, dep_vars) -> xr.Dataset:
+
+    def get_in_ds(self) -> xr.Dataset:
         """Generate an input Dataset for the model"""
-        beta = dep_vars[0]
         return xs.create_setup(
             model=self.model,
             clocks={'step': self.step_clock},
             input_vars={
                 'read_config__config_fp': self.config_fp,
-                'foi__beta': beta
+                'foi__beta': self.xvars[0]
             },
             output_vars={'apply_counts_delta__counts': 'step'}
         )
     
-    def get_out_ds(self, dep_vars) -> xr.Dataset:
+    def get_out_ds(self) -> xr.Dataset:
         """Run model, returning output Dataset"""
         return (self 
-                .get_in_ds(dep_vars) 
+                .get_in_ds() 
                 .xsimlab 
                 .run(model=self.model, decoding=dict(mask_and_scale=False)))
-    
-    def calc_residual(self, dep_vars, ih_actual) -> float:
+
+    def calc_residual(self, xvars) -> float:
+        # Store current x variable values in attr
+        self.xvars = xvars
         # Run model and pull out counts of Ih compartment over time
         ih_pred = (self
-                   .get_out_ds(dep_vars) 
+                   .get_out_ds() 
                    .apply_counts_delta__counts 
                    .loc[dict(compartment='Ih')] 
                    .sum(dim=['age_group', 'risk_group', 'vertex']))
@@ -88,5 +93,5 @@ class LeastSqFitter:
         assert 'step' in ih_pred.dims, f"'step' is not in {ih_pred.dims}"
 
         # Calculate residual
-        resi = ih_pred - ih_actual
+        resi = ih_pred - self.data
         return resi
--- tests/test_fit/test_llsq.py
+++ tests/test_fit/test_llsq.py
@@ -2,22 +2,22 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from scipy.optimize.optimize import OptimizeResult
-from episimlab.fit.llsq import LeastSqFitter
+from episimlab.fit.llsq import FitBetaFromHospHeads
 from episimlab.models import basic as basic_models
 
 
 def test_fit_llsq():
     model = basic_models.cy_seir_cy_foi().drop_processes(['setup_beta'])
     step_clock = pd.date_range(start='2/1/2020', end='4/1/2020', freq='12H')
-    result = (
-        LeastSqFitter(
-            model=model, 
-            data=0., 
-            step_clock=step_clock, 
-            guess=0.035, 
-            verbosity=2)
-        .fit()
+    fitter = FitBetaFromHospHeads(
+        model=model, 
+        data_fp='tests/data/ll_hosp_delta.csv', 
+        step_clock=step_clock, 
+        guess=0.035, 
+        verbosity=2
     )
-    assert isinstance(result, OptimizeResult)
-    assert result['success']
-    assert result['x'] < 1e-7 
+    fitter.data = 0.
+    soln = fitter.run()
+    assert isinstance(soln, OptimizeResult)
+    assert soln['success']
+    assert soln['x'] < 1e-7 
