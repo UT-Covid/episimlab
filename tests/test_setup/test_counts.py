ddb72e9cb894e4452a771d39ee11d12cb6e00fdd
Ethan Ho <eho@tacc.utexas.edu>
2021-05-13 16:05:14 -0500

    Modify age group coords for tests with census data

--- tests/test_setup/test_counts.py
+++ tests/test_setup/test_counts.py
@@ -1,5 +1,5 @@
 import pytest
-import pandas as pd
+import xarray as xr
 from episimlab.setup.counts import InitCountsFromCensusCSV
 
 
@@ -13,9 +13,13 @@ class TestInitCountsFromCensusCSV:
     def test_can_initialize(self, census_counts_csv, counts_coords):
         """
         """
+        age_coords = counts_coords['age_group']
+        # census data uses <5 instead of 0-4
+        age_coords[age_coords.index('0-4')] = '<5'
         inputs = {
-            'vertex': counts_coords['vertex'],
-            'age_group': counts_coords['age_group'],
+            # must be zcta that exist in census_counts_csv
+            'vertex': [75001, 75002, 79312],
+            'age_group': age_coords,
             'risk_group': counts_coords['risk_group'],
             'compartment': counts_coords['compartment'],
             'census_counts_csv': census_counts_csv,
