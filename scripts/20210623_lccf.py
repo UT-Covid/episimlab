9b0760fd8767cdbe2e575f100e9f092288eda46b
Ethan Ho <eho@tacc.utexas.edu>
2021-06-23 12:38:33 -0500

    Clean up and add Dask profiling

--- scripts/20210623_lccf.py
+++ scripts/20210623_lccf.py
@@ -4,10 +4,11 @@ import pandas as pd
 import xarray as xr
 import xsimlab as xs
 from matplotlib import pyplot as plt
-from episimlab import apply_counts_delta
 from episimlab.models import basic
+from episimlab.pytest_utils import profiler
 from episimlab.partition.partition import Partition2Contact
-from episimlab.setup import coords, counts
+from episimlab.setup import counts
+
 
 @xs.process
 class InitCountsCustom(counts.InitCountsFromCensusCSV):
@@ -18,6 +19,7 @@ class InitCountsCustom(counts.InitCountsFromCensusCSV):
         self.counts.loc[dict(compartment='Ia', risk_group='low')] = self.initial_ia
 
 
+@profiler(flavor='dask', log_dir='./logs', show_prof=True)
 def partition_from_csv(**opts) -> xr.Dataset:
     model = (basic
              .partition()
@@ -45,6 +47,7 @@ def partition_from_csv(**opts) -> xr.Dataset:
     )
     # breakpoint()
     out_ds = run_model(input_ds, model)
+    
     return out_ds
 
 
@@ -55,41 +58,6 @@ def xr_viz(data_array, sel=dict(), isel=dict(), timeslice=slice(0, None),
     isel.update({'step': timeslice})
     da = data_array[isel].loc[sel].sum(dim=sum_over)
     _ = da.plot.line(x='step', aspect=2, size=7)
-
-
-def partition_from_nc() -> xr.Dataset:
-    raise NotImplementedError
-
-    model = (basic
-             .partition()
-             .drop_processes(['setup_beta'])
-             .update_processes(dict(setup_counts=counts.InitCountsFromCensusCSV))
-            )
-
-    input_vars = {
-        'config_fp': 'scripts/20210512_partition_model.yaml',
-        'contact_da_fp': 'tests/data/20200311_contact_matrix.nc',
-        'census_counts_csv': 'data/2019_zcta_pop_5_age_groups.csv',
-        'beta': 1.
-    }
-    # Reindex with `process__variable` keys
-    input_vars_with_proc = dict()
-    for proc, var in model.input_vars:
-        assert var in input_vars, f"model requires var {var}, but could not find in input var dict"
-        input_vars_with_proc[f"{proc}__{var}"] = input_vars[var]
-
-    # run model
-    input_ds = xs.create_setup(
-        model=model,
-        clocks={
-            'step': pd.date_range(start='3/11/2020', end='6/1/2020', freq='24H')
-        },
-        input_vars=input_vars_with_proc,
-        output_vars=dict(apply_counts_delta__counts='step')
-    )
-    # breakpoint()
-    out_ds = run_model(input_ds, model)
-    return out_ds
 
 
 def run_model(input_ds: xr.Dataset, model: xs.Model) -> xr.Dataset:
