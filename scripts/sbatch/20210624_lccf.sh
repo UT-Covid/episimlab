239b6b19b86e728af5f24c2edf88f7a4ea41740e
Ethan Ho <eho@tacc.utexas.edu>
2021-07-01 16:32:28 -0500

    sbatch script passes list n_cores

--- scripts/sbatch/20210624_lccf.sh
+++ scripts/sbatch/20210624_lccf.sh
@@ -11,7 +11,7 @@
 #SBATCH -p development          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 02:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 01:00:00        # Run time (hh:mm:ss)
 
 # Any other commands must follow all #SBATCH directives...
 module load python3/3.7.0 gsl/2.6
@@ -42,8 +42,8 @@ python3 -m pip freeze
 # run script
 OPTS=
 OPTS="$OPTS --config-fp scripts/20210625_lccf.yaml"
-OPTS="$OPTS --travel-fp data/lccf/travel0.csv"
-OPTS="$OPTS --contacts-fp data/lccf/contacts0.csv"
-OPTS="$OPTS --census-counts-csv data/lccf/census0.csv"
-OPTS="$OPTS --n-cores-partition 8"
+OPTS="$OPTS --travel-fp data/lccf/travel_pop1_rows1.csv"
+OPTS="$OPTS --contacts-fp data/lccf/contacts_pop1_rows1.csv"
+OPTS="$OPTS --census-counts-csv data/lccf/census_pop1_rows1.csv"
+OPTS="$OPTS --n-cores-partition 1,2,4,8,16,32"
 PYTHONPATH='.' python3 scripts/20210623_lccf.py $OPTS
