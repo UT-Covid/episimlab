7e45e0314b21e147295fe85764651c2739342156
Ethan Ho <eho@tacc.utexas.edu>
2021-07-27 14:46:30 -0500

    Passing consistency tests with FOI and SEIR separate

--- tests/test_seir/test_seir_with_foi.py
+++ tests/test_seir/test_seir_with_foi.py
@@ -1,7 +1,9 @@
 import pytest
 import logging
 import xarray as xr
+import xsimlab as xs
 from episimlab.seir.seir_with_foi import SEIRwithFOI
+from episimlab.models.basic import cy_seir_cy_foi, seir_with_foi
 
 
 class TestSEIRwithFOI:
@@ -30,3 +32,38 @@ class TestSEIRwithFOI:
 
         # TODO
         # assert not result.isnull().any()
+
+    @pytest.mark.parametrize('model1, model2', [
+        (cy_seir_cy_foi(), seir_with_foi()),
+    ])
+    def test_consistent_with_foi_separate(self, step_clock, model1, model2, 
+                                          config_fp, config_dict):
+        """TestCompareBasicModels.test_seir_foi_combos_deterministic, but
+        allows for comparison of models with no `foi` process in the model.
+        """
+        # generate input variables
+        cfg = config_fp(config_dict)
+        input_vars = dict(
+            read_config__config_fp=cfg,
+            setup_coords__config_fp=cfg
+        )
+
+        # shared inputs based on the default model
+        out_var_key = 'apply_counts_delta__counts'
+        in_ds = xs.create_setup(
+            model=model1,
+            clocks={k: step_clock[k] for k in step_clock},
+            input_vars=input_vars,
+            output_vars={out_var_key: 'step'}
+        )
+
+        # run both models
+        result1 = in_ds.xsimlab.run(
+            model=model1, decoding=dict(mask_and_scale=False))[out_var_key]
+        result2 = in_ds.xsimlab.run(
+            model=model2, decoding=dict(mask_and_scale=False))[out_var_key]
+
+        # check typing and equality
+        assert isinstance(result1, xr.DataArray)
+        assert isinstance(result2, xr.DataArray)
+        xr.testing.assert_allclose(result1, result2)
