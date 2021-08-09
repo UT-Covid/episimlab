4f5bcc40f0e9e12adee8359d177688021da91187
Ethan Ho <eho@tacc.utexas.edu>
2021-08-09 14:44:49 -0500

    trim_data_to_coords in utils

--- episimlab/utils/variable.py
+++ episimlab/utils/variable.py
@@ -19,3 +19,24 @@ def get_var_dims(process, name) -> tuple:
 
 def group_dict_by_var(d: dict) -> dict:
     return {k: d[(proc, k)] for (proc, k) in d}
+
+
+def trim_data_to_coords(data, coords):
+    """Be sure to check that order of dims in data is same as order of
+    dims in coords.
+    """
+    assert isinstance(coords, list)
+    assert isinstance(data, np.ndarray)
+    expected_shape = [len(c) for dim, c in coords]
+    # check that N-D is same
+    assert len(data.shape) == len(expected_shape)
+    # list of slices, one for each dimension
+    slices = list()
+    for dc, ec in zip(data.shape, expected_shape):
+        if ec < dc:
+            cut = ec
+        else:
+            cut = None
+        slices.append(slice(None, cut))
+    # trim data to expected shape
+    return data.__getitem__(slices)
