d76d31344b1c5d3366368ccadbc92a985ba21902
Kelly Pierce <kellyannepierce@gmail.com>
2021-10-18 13:09:06 -0500

    replace contact matrix with travel and census dataframes

--- examples/vaccine_v1.py
+++ examples/vaccine_v1.py
@@ -1,11 +1,16 @@
 from episimlab.models import Vaccine
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
 
 def main():
     model = Vaccine()
     model.run(input_vars={
         # update default input vars
         'sto_toggle': 0,
-        'contact_da_fp': 'data/20200311_contact_matrix.nc',
+        'contacts_fp': '/Users/kpierce/COVID19/SchoolCatchmentDemo/mock_polymod_contacts.csv',
+        'travel_fp': '/Users/kpierce/COVID19/SchoolCatchmentDemo/spring_semester_2020_travel.csv',
+        'initial_state_df': '/Users/kpierce/COVID19/SchoolCatchmentDemo/mock_initial_state_census.csv'
     })
     final_state = model.out_ds['compt_model__state']
     print(final_state.coords)
